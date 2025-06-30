8cacf71767073d07c71174638028ca42212fa2fa
RGV <ricardo.guzman.velasco@outlook.com>
2025-06-30 12:50:30 +0200

    docs: detect opportunity to begin with

--- C#/Trivia/Tests/TriviaCharacterizationTests.cs
+++ C#/Trivia/Tests/TriviaCharacterizationTests.cs
@@ -4,6 +4,7 @@ namespace Tests;
 
 public class TriviaCharacterizationTests
 {
+    // Step 1: the easiest test is the one from the playability of the game.
     [Fact]
     public void JustRun()
     {
